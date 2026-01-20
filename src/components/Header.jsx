f398c7391517c1c846d5687db3219d51b7727632
Rick Soze <rick.soze.001@gmail.com>
2026-01-20 11:56:33 +0100

    Fix navbar transparency issue

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -38,7 +38,7 @@ const Header = () => {
       animate={{ y: 0, opacity: 1 }}
       transition={{ duration: 0.5 }}
       className={`fixed top-0 left-0 right-0 w-full z-50 transition-all duration-300 ${
-        isScrolled ? 'glass' : 'bg-transparent'
+        isScrolled || isMenuOpen ? 'glass' : 'bg-transparent'
       }`}
       role="banner"
     >
